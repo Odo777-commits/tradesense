463d9ce33a0d4220f295d6c90db0379d10668db5
Odinoooo <mohammadjavad.khalili777@gmail.com>
2025-04-20 11:49:33 +0330

    Connect backend to database

--- db/pool.js
+++ db/pool.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = new Pool({
     user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'Trade_Sense',
+    database: process.env.DB_NAME || 'railway',
     password: process.env.DB_PASSWORD || 'Javad111',
     port: process.env.DB_PORT || 5432,
 });
